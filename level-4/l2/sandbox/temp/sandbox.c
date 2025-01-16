b843c310373d427bdd2118d84889de12dfbf8082
Sanghun Park <po4865@gmail.com>
2025-01-16 17:39:15 +0100

    update: Update solution to cover test code

--- level-4/l2/sandbox/temp/sandbox.c
+++ level-4/l2/sandbox/temp/sandbox.c
@@ -5,15 +5,20 @@
 #include <string.h>
 #include <stdbool.h>
 
-static void timeout_handler(int sig)
+pid_t pid;
+bool kill_failed = false;
+bool killed_child = false;
+
+static void kill_proc(int sig)
 {
 	(void)sig;
-	_exit(SIGALRM);
+	if (kill(pid, SIGKILL) == -1)
+		kill_failed = true;
+	killed_child = true;
 }
 
 int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 {
-	pid_t pid;
 	int status;
 	struct sigaction sa = {0};
 
@@ -23,23 +28,33 @@ int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 
 	if (pid == 0)
 	{
-		sa.sa_handler = timeout_handler;
-		sa.sa_flags = 0;
-		if (sigaction(SIGALRM, &sa, NULL) < 0)
-			_exit(1);
-		alarm(timeout);
 		f();
 		_exit(0);
 	}
-
+	sa.sa_handler = kill_proc;
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGALRM, &sa, NULL))
+		return -1;
+	alarm(timeout);
 	pid_t wresult = waitpid(pid, &status, WUNTRACED);
 	if (wresult < 0)
+		return -1;
+	if (kill_failed)
 		return -1;
 	if (WIFSIGNALED(status))
 	{
 		int sig = WTERMSIG(status);
-		if (verbose)
-			printf("bf: termsig %s\n", strsignal(sig));
+		if (sig == SIGKILL && killed_child)
+		{
+			if (verbose)
+				printf("bf: timeout %u seconds\n", timeout);
+			return 0;
+		}
+		else
+		{
+			if (verbose)
+				printf("bf: termsig %s\n", strsignal(sig));
+		}
 		return 0;
 	}
 	else if (WIFSTOPPED(status))
@@ -57,12 +72,6 @@ int sandbox(void (*f)(void), unsigned int timeout, bool verbose)
 			if (verbose)
 				printf("nf\n");
 			return 1;
-		}
-		else if (code == SIGALRM)
-		{
-			if (verbose)
-				printf("bf: timeout %u seconds\n", timeout);
-			return 0;
 		}
 		else
 		{
